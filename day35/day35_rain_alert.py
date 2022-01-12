0932021bc9266c7cc3fec21a6292cfc97b5f3cb6
mskmee <mskmee2@gmail.com>
2022-01-12 17:06:42 +0200

    day35 update

---
+++ day35/day35_rain_alert.py
@@ -0,0 +1,34 @@
+from day35_start import API_KEY, MY_LAT, MY_LONG
+import requests
+import smtplib
+from data import user_name, password
+
+
+EMAIL = user_name
+PASSWORD = password
+
+parameters = {
+    'lat': MY_LAT,
+    'lon': MY_LONG,
+    'exclude': 'current,minutely,daily',
+    'units': 'metric',
+    'appid': API_KEY
+}
+
+response = requests.get('https://api.openweathermap.org/data/2.5/onecall', params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['hourly'][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL, to_addrs='mskmee2@gmail.com', msg='The rain shall come.\nTake umbrella')
